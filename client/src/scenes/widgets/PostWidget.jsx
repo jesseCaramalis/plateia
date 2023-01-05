f1de947698ee6e4371f28ec685329e07232a680b
Jesse Caramalis <101160675+jesseCaramalis@users.noreply.github.com>
2023-01-05 17:59:15 +1000

    Post Widgets

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -6,3 +6,32 @@ import {
 import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
 import FlexBetween from "components/FlexBetween";
 import Friend from "components/Friend";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPost } from "state";
+
+const PostWidget = ({
+        postId,
+        postUserId,
+        name,
+        description,
+        location,
+        picturePath,
+        userPicturePath,
+        likes,
+        comments,
+}) => {
+    const [isComments, setIsComments] = useState(false);
+    const dispatch = useDispatch();
+    const token = useSelector((state) => state.token);
+    const loggedInUserId = useSelector((state) => state.user._id);
+    const isLiked = Boolean(likes[loggedInUserId]);
+    const likeCount = Object.keys(likes).length;
+
+    const { palette } = useTheme();
+    const primary = palette.primary.main;
+    const main = palette.neutral.main;
+}
+
+export default PostWidget;
