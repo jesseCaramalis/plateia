0c208880be95c8223405b8ccac02387aee418374
Jesse Caramalis <101160675+jesseCaramalis@users.noreply.github.com>
2023-01-10 11:43:03 +1000

    deployment fixes

--- server/index.js
+++ server/index.js
@@ -59,9 +59,6 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
-app.use(express.static('./client/build'));
-app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
-
 // MONGOOSE
 const PORT = process.env.PORT || 6001;
 mongoose.set('strictQuery', true)
